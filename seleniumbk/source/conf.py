9536f2cc640f634134caa56a11d357daae7094ea
yanliang <18297498080@163.com>
2020-05-25 15:44:33 +0800

    Update conf.py

--- seleniumbk/source/conf.py
+++ seleniumbk/source/conf.py
@@ -66,5 +66,30 @@ master_doc = 'index'
 
 html_sidebars = { '**': ['globaltoc.html', 'sourcelink.html', 'searchbox.html']}
 
+
+latex_engine = 'xelatex'
+latex_elements = {
+    'papersize': 'a4paper',
+    'pointsize': '11pt',
+    'preamble': r'''
+\usepackage{xeCJK}
+\setCJKmainfont[BoldFont=STZhongsong, ItalicFont=STKaiti]{STSong}
+\setCJKsansfont[BoldFont=STHeiti]{STXihei}
+\setCJKmonofont{STFangsong}
+\XeTeXlinebreaklocale "zh"
+\XeTeXlinebreakskip = 0pt plus 1pt
+\parindent 2em
+\definecolor{VerbatimColor}{rgb}{0.95,0.95,0.95}
+\setcounter{tocdepth}{3}
+\renewcommand\familydefault{\ttdefault}
+\renewcommand\CJKfamilydefault{\CJKrmdefault}
+'''
+}
+# 设置文档
+latex_documents = [
+    (master_doc, 'sphinx.tex', '你的第一本 Sphinx 书',
+     '作者：qiwihui', 'manual', True),
+]
+
 _exts = "../exts"
 sys.path.append(os.path.abspath(_exts))
